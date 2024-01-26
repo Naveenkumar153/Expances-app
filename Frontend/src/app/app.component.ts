d4ea109953d9497bbb4bc62d75435f3cc7fecc07
Naveenkumar153 <naveenkumartech2025@gmail.com>
2024-01-26 20:38:38 +0530

    build: add angular material

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ANGULAR_MATERIAL_COMPONENTS } from './material.componets';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ANGULAR_MATERIAL_COMPONENTS
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
